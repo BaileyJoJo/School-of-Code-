f8c3972e5fc9f8ed2b447a97bf075da9f2e42097
BaileyJoJo <127659714+BaileyJoJo@users.noreply.github.com>
2023-03-23 12:29:17 +0000

    Add files via upload
    W1D3

---
+++ bc14_w1d3_workshop_js-101-rm-52/main.js
@@ -0,0 +1,22 @@
+//Task 1
+let dinosaurs = 6500000
+console.log (dinosaurs)
+
+dinosaurs = dinosaurs + 50
+console.log (dinosaurs)
+
+dinosaurs = dinosaurs * 2
+console.log (dinosaurs)
+
+dinosaurs = 6500000
+
+//Task 2
+let dinosaursBeDead =  " years ago"
+
+dinosaursBeDead = (dinosaurs + dinosaursBeDead)
+console.log (dinosaursBeDead)
+
+//Task 3
+let meteor = " the cute dinos got wiped out"
+let history = (dinosaurs + dinosaursBeDead + meteor)
+console.log (history)
